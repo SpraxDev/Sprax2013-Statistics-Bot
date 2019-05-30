26092a30ab1c71492fe3660d53f8bf5f21d76301
Sprax2013 <Developer@sprax2013.de>
2019-05-30 20:01:50 +0200

    Create index.js
    Updates WorkShopBrowser and SkinDB

---
+++ index.js
@@ -0,0 +1,200 @@
+const request = require('request'),
+  fs = require('fs'),
+  Discord = require('discord.js');
+
+let data = getData();
+const botToken = data['BotToken'];
+
+updateWSB();
+updateSkinDB();
+
+setInterval(updateWSB, 1000 * 60 * 15);
+setInterval(updateSkinDB, 1000 * 60 * 15);
+
+const client = new Discord.Client();
+client.on('error', (err) => {
+  console.error(new Date() + ": Discord client encountered an error");
+  console.error(err);
+});
+
+client.login(botToken);
+
+function updateWSB() {
+  request('https://workshopbrowser.com/stats', {
+    headers: {
+      Accept: 'application/json'
+    }
+  }, (err, res, body) => {
+    if (err) return console.error(err);
+    if (res.statusCode !== 200) return console.error('API-Request failed: ', body)
+
+    body = JSON.parse(body);
+
+    for (const channelID in data['Stats']['WSB']) {
+      if (data['Stats']['WSB'].hasOwnProperty(channelID)) {
+        const lastMsgID = data['Stats']['WSB'][channelID];
+
+        let reqBody = JSON.stringify({
+          embed: {
+            title: '**Statistics | Workshop Browser**',
+            url: 'https://workshopbrowser.com/stats',
+            color: 16738378,
+            timestamp: new Date().toISOString(),
+            thumbnail: {
+              url: 'https://workshopbrowser.com/img/favicons/android-chrome-256x256.png'
+            },
+            author: {
+              name: 'Workshop Browser',
+              url: 'https://workshopbrowser.com/',
+              icon_url: 'https://workshopbrowser.com/img/favicons/android-chrome-256x256.png'
+            },
+            fields: [
+              {
+                name: 'Games',
+                value: `${body['estGameCount']}`
+              },
+              {
+                name: 'User',
+                value: `${body['estUserCount']}`,
+                inline: true
+              },
+              {
+                name: 'User with Uploads',
+                value: `${body['userCountWithUploads']}`,
+                inline: true
+              },
+              {
+                name: 'Files uploaded',
+                value: `${body['estFileUploads']}`
+              }
+            ]
+          }
+        });
+
+        request(`https://discordapp.com/api/channels/${channelID}/messages/${lastMsgID || -1}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bot ${botToken}`
+          },
+          body: reqBody
+        }, (err, res, _body) => {
+          if (err) return console.error(err);
+
+          if (res.statusCode !== 200) {
+            request('https://discordapp.com/api/channels/582799725435158528/messages', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bot ${botToken}`
+              },
+              body: reqBody
+            }, (err, res, body) => {
+              if (err) return console.error(err);
+
+              if (res.statusCode !== 200) return console.error('API-Request failed: ', body);
+
+              data['Stats']['WSB'][channelID] = JSON.parse(body).id;
+              setData();
+            });
+          }
+        });
+      }
+    }
+  });
+}
+
+function updateSkinDB() {
+  request('https://api.skindb.net/stats', (err, res, body) => {
+    if (err) return console.error(err);
+    if (res.statusCode !== 200) return console.error('API-Request failed: ', body)
+
+    body = JSON.parse(body);
+
+    for (const channelID in data['Stats']['SkinDB']) {
+      if (data['Stats']['SkinDB'].hasOwnProperty(channelID)) {
+        const lastMsgID = data['Stats']['SkinDB'][channelID];
+
+        let reqBody = JSON.stringify({
+          embed: {
+            title: '**Statistics | SkinDB**',
+            url: 'https://SkinDB.net/',
+            color: 16738378,
+            timestamp: new Date().toISOString(),
+            thumbnail: {
+              url: 'https://skindb.net/img/Logo.png'
+            },
+            author: {
+              name: 'Workshop Browser',
+              url: 'https://SkinDB.net/',
+              icon_url: 'https://skindb.net/img/Logo.png'
+            },
+            fields: [
+              {
+                name: 'Skins',
+                value: `${body['estSkinCount']}`,
+                inline: true
+              },
+              {
+                name: 'Duplicates',
+                value: `${body['duplicateSkinCount']}`,
+                inline: true
+              }
+            ]
+          }
+        });
+
+        request(`https://discordapp.com/api/channels/${channelID}/messages/${lastMsgID || -1}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bot ${botToken}`
+          },
+          body: reqBody
+        }, (err, res, _body) => {
+          if (err) return console.error(err);
+
+          if (res.statusCode !== 200) {
+            request(`https://discordapp.com/api/channels/${channelID}/messages`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bot ${botToken}`
+              },
+              body: reqBody
+            }, (err, res, body) => {
+              if (err) return console.error(err);
+
+              if (res.statusCode !== 200) return console.error('API-Request failed: ', body);
+
+              data['Stats']['SkinDB'][channelID] = JSON.parse(body).id;
+              setData();
+            });
+          }
+        });
+      }
+    }
+  });
+}
+
+function getData() {
+  if (fs.existsSync('./storage/data.json')) {
+    return JSON.parse(fs.readFileSync('./storage/data.json', { encoding: 'UTF-8' }));
+  }
+
+  fs.writeFileSync('./storage/data.json', JSON.stringify({
+    BotToken: "BOT_TOKEN_HERE",
+
+    Stats: {
+      WSB: {
+
+      }
+    }
+  }));
+  setData();
+  return getData();
+}
+
+function setData() {
+  fs.writeFileSync('./storage/data.json', JSON.stringify(data));
+}
